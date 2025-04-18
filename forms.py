a1f8a0875f5df96d8e5a5d836bda3de39f0f35b9
vs2212 <salatovaveronika22@gmail.com>
2025-04-18 11:55:15 +0500

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,6 +1,55 @@
-# forms.py
 from django import forms
-from .models import Book
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User  # Импортируем стандартную модель User
+from .models import Book, UserProfile
+from django import forms
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError  # Добавьте этот импорт
+
+User = get_user_model()
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+class SignUpForm(forms.ModelForm):
+    username = forms.CharField(max_length=255, required=True)
+    email = forms.EmailField(max_length=254, required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Пароль")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Подтверждение пароля")
+
+    class Meta:
+        model = User  # Используем текущую модель пользователя
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        # Проверяем, что email уникален
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Пользователь с таким email уже существует.")
+        return email
+
+    def clean_password2(self):
+        # Проверяем, что пароли совпадают
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Пароли не совпадают")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])  # Сохраняем зашифрованный пароль
+        if commit:
+            user.save()
+        return user
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=255, required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User  # Используем стандартную модель User
+        fields = ('username', 'password')
 
 class BookForm(forms.ModelForm):
     class Meta:
--- views.py
+++ views.py
@@ -1,14 +1,95 @@
+import json
+
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm, SignUpForm, LoginForm
 from django.core.paginator import Paginator
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+
+
+def check_username(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        User = get_user_model()  # Получаем кастомную модель пользователя
+        exists = User.objects.filter(username=username).exists()
+        return JsonResponse({'exists': exists})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+def check_username(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        User = get_user_model()  # Получаем кастомную модель пользователя
+        exists = User.objects.filter(username=username).exists()
+        return JsonResponse({'exists': exists})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+def user_logout(request):
+    logout(request)
+    return redirect('book_list')
+
+def is_admin(user):
+    return user.role == 'admin'
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Сохраняем пользователя
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # Аутентифицируем пользователя
+            if user is not None:
+                login(request, user)  # Входим в систему
+                return redirect('book_list')  # Перенаправляем на главную страницу
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('book_list')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
 
 # Отображение списка книг
 def book_list(request):
-    books_list = Book.objects.all().order_by('title')  # Сортировка по названию
-    paginator = Paginator(books_list, 2)  # 2 элемента на странице
+    books = Book.objects.all()
+
+    # Фильтрация по автору
+    author = request.GET.get('author')
+    if author:
+        books = books.filter(author__icontains=author)
+
+    # Фильтрация по названию
+    title = request.GET.get('title')
+    if title:
+        books = books.filter(title__icontains=title)
+
+    # Сортировка по цене
+    sort = request.GET.get('sort')
+    if sort == 'price_desc':
+        books = books.order_by('-price')  # Сортировка по убыванию цены
+    elif sort == 'price_asc':
+        books = books.order_by('price')  # Сортировка по возрастанию цены
+
+    # Пагинация
+    paginator = Paginator(books, 5)  # 5 книг на странице
     page_number = request.GET.get('page')
     books = paginator.get_page(page_number)
+
     return render(request, 'books/book_list.html', {'books': books})
 
 # Отображение деталей книги
@@ -17,21 +98,19 @@ def book_detail(request, pk):
     return render(request, 'books/book_detail.html', {'book': book})
 
 # Добавление новой книги
+@login_required
 def book_new(request):
     if request.method == "POST":
-        print("Данные формы:", request.POST)  # Отладочный вывод
         form = BookForm(request.POST)
         if form.is_valid():
-            print("Форма валидна, сохраняем книгу")  # Отладочный вывод
             form.save()
             return redirect('book_list')
-        else:
-            print("Форма невалидна:", form.errors)  # Отладочный вывод
     else:
         form = BookForm()
     return render(request, 'books/book_edit.html', {'form': form})
 
 # Редактирование существующей книги
+@user_passes_test(is_admin)
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == "POST":
@@ -44,6 +123,7 @@ def book_edit(request, pk):
     return render(request, 'books/book_edit.html', {'form': form})
 
 # Удаление книги
+@user_passes_test(is_admin)
 def book_delete(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book.delete()
