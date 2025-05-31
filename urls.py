ba8b276dbb2c899aabf18e96d1348d4c07743340
vs2212 <salatovaveronika22@gmail.com>
2025-05-31 13:44:19 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,24 @@
-# urls.py
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+"""
+URL configuration for echoserver project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('logout/', views.user_logout, name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.user_login, name='login'),
-    path('', views.book_list, name='book_list'),
-    path('book/new/', views.book_new, name='book_new'),
-    path('book/<int:pk>/', views.book_detail, name='book_detail'),
-    path('book/<int:pk>/edit/', views.book_edit, name='book_edit'),
-    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
-    path('check-username/', views.check_username, name='check_username'),
+    path('admin/', admin.site.urls),
+    path('', include('echo.urls')),
+
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
